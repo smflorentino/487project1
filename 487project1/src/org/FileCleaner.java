605ad5c5ca3ad43061f410191fc4d97cfa33a01c
smflorentino <smflorentino@server.fake>
2013-02-17 21:31:17 -0500

    File Cleaner Working Now!!!

--- 487project1/src/org/FileCleaner.java
+++ 487project1/src/org/FileCleaner.java
@@ -1,11 +1,17 @@
 package org;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
+
+
 public class FileCleaner {
+	
+	
 	public static void main(String[] args) {
 		FileCleaner f = new FileCleaner();
 		try {
@@ -17,16 +23,25 @@ public class FileCleaner {
 	}
 	public void start() throws IOException {
 		String fileName = JOptionPane.showInputDialog("Enter file name to clean");
-		Scanner sc = new Scanner(fileName);
+		BufferedReader sc = new BufferedReader(new FileReader(fileName));
 		FileWriter fs=  new FileWriter(fileName+"clean");
 		BufferedWriter out = new BufferedWriter(fs);
 		
 		String line;
-		while(sc.hasNextLine()) {
-			line=sc.nextLine();
-			if(this.isValidLine(line)) {
-				out.write(line);
+		int linenum=0;
+		while(true) {
+			//System.out.println("Line:" + linenum);
+			line=sc.readLine();
+			if(line == null) {
+				break;
 			}
+			if(this.isValidLine(line)) {
+				out.write(line+'\n');
+			}
+			else {
+			//	System.out.println("Found invalid line" + line);
+			}
+			linenum++;
 		}
 		out.close();
 	}
@@ -34,13 +49,22 @@ public class FileCleaner {
 	private boolean isValidLine(String s) {
 		char c;
 		for(int i=0;i<s.length();i++) {
-			c=Character.toLowerCase(s.charAt(i));
-			if(c<'a' || c>'z') {
-				if(c<'0' || c>'9') {
-					return false;
-				}
+			c=s.charAt(i);
+			if(!(this.isValidCharacter(c))) {
+				
+				return false;
 			}
 		}
+		//System.out.println("found valid line:");
 		return true;
+	}
+	
+	private boolean isValidCharacter(char c) {
+		//.out.println(c+0);
+		if(c >=32 && c<=126) {
+			//System.out.println("Special");
+			return true;
+		}
+		return false;
 	}
 }
